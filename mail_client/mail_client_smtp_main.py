fcbdba1d7ca2879dd3618961eccbe1fb88c5ac67
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2023-11-22 19:22:40 +0700

    thêm file function cho pop3; thêm một số hàm; điểu chỉnh hàm main để test

--- mail_client/mail_client_smtp_main.py
+++ mail_client/mail_client_smtp_main.py
@@ -1,14 +1,20 @@
 from socket import *
 from mail_client_smtp_func import *
+from mail_client_pop3_func import *
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver : tuple = ("127.0.0.1", 2225)
+smtpMailserver : tuple = ("127.0.0.1", 2225)
+pop3Mailserver : tuple = ("127.0.0.1", 3335)
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
 #AF_INET = IPv4, SOCK_STREAM = TCP
-clientSocket : socket = initiate(mailserver)
-usermail : str = 'sender@test.net'
 
+#todo: change socket base on the user's need: send / retrive mail, default is retrival -Phuong
+clientSocket : socket = initiate(mailserver)
+#usermail : str = 'sender@test.net'
+
+usermail : str = "user@test.net"
+login(clientSocket)
 draftMail(clientSocket, usermail)
 
 
