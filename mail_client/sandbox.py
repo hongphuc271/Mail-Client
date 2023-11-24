c2a7f6efd0b4bdbe00c7e0a594fb94af0471a257
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2023-11-24 09:26:55 +0700

    Revert "thêm 1 số hàm về ui"
    This reverts commit 9375cd4b449aebac9bc773a0e77c1c089544e97d.

--- mail_client/sandbox.py
+++ mail_client/sandbox.py
@@ -1,23 +1,20 @@
 import tkinter as tk
-from mail_client_gui_func import *
 
-#def get_text_content():
+def get_text_content():
     # Lấy nội dung từ Text widget
-#    content = text_widget.get("1.0", "end-1c")
-#    print("Content:", content)
+    content = text_widget.get("1.0", "end-1c")
+    print("Content:", content)
 
 # Tạo cửa sổ
-#window = tk.Tk()
+window = tk.Tk()
 
 # Tạo Text widget
-#text_widget = tk.Text(window, height=5, width=30)
-#text_widget.pack()
+text_widget = tk.Text(window, height=5, width=30)
+text_widget.pack()
 
 # Tạo nút để lấy nội dung
-#get_button = tk.Button(window, text="Get Content", command=get_text_content)
-#get_button.pack()
+get_button = tk.Button(window, text="Get Content", command=get_text_content)
+get_button.pack()
 
 # Bắt đầu vòng lặp sự kiện chính
-#window.mainloop()
-
-app()
+window.mainloop()
